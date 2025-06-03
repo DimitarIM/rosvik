30a0b24a8b235487c654527a48186b80685600af
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-03 23:24:05 +0200

    Add teamData

---
+++ src/data/teamData.ts
@@ -0,0 +1,146 @@
+const ManagementTeamData = [
+  {
+    title: "Producer",
+    name: "Axel Lindberg",
+    linkedin: "linkedin",
+    discord: "discord",
+    website: "website",
+    games: [],
+  },
+  {
+    title: "Project manager",
+    name: "Camilla Rengbrand",
+    linkedin: "linkedin",
+    discord: "discord",
+    website: "website",
+    games: [],
+  },
+  {
+    title: "Release manager",
+    name: "Daniel Zangger Borch",
+    linkedin: "linkedin",
+    discord: "discord",
+    website: "website",
+    games: [],
+  },
+];
+
+const DesignTeamData = [
+  {
+    title: "PO/Gameplay & Tech designer",
+    name: "Daniel Karlberg",
+    linkedin: "linkedin",
+    discord: "discord",
+    website: "website",
+    games: [],
+  },
+  {
+    title: "Generalist designer",
+    name: "Sebastian Wallin",
+    linkedin: "linkedin",
+    discord: "discord",
+    website: "website",
+    games: [],
+  },
+  {
+    title: "Gameplay & Level designer",
+    name: "Marcus Lindberg",
+    linkedin: "linkedin",
+    discord: "discord",
+    website: "website",
+    games: [],
+  },
+];
+
+const ArtTeamData = [
+  {
+    title: "Art director",
+    name: "Andreas “Ato” Holewa",
+    linkedin: "linkedin",
+    discord: "discord",
+    website: "website",
+    games: [],
+  },
+  {
+    title: "Environment artist",
+    name: "Judith Gammelli",
+    linkedin: "linkedin",
+    discord: "discord",
+    website: "website",
+    games: [],
+  },
+  {
+    title: "Prop & Character artist",
+    name: "Finn Mazur",
+    linkedin: "linkedin",
+    discord: "discord",
+    website: "website",
+    games: [],
+  },
+  {
+    title: "General game artist",
+    name: "Wera Skoglund",
+    linkedin: "linkedin",
+    discord: "discord",
+    website: "website",
+    games: [],
+  },
+  {
+    title: "VFX artist",
+    name: "Antonia Mertes",
+    linkedin: "linkedin",
+    discord: "discord",
+    website: "website",
+    games: [],
+  },
+];
+
+const ProgrammersData = [
+  {
+    title: "Programmer",
+    name: "Anton Lanning",
+    linkedin: "linkedin",
+    discord: "discord",
+    website: "website",
+    games: [],
+  },
+  {
+    title: "Programmer",
+    name: "Venessa Sjölin",
+    linkedin: "linkedin",
+    discord: "discord",
+    website: "website",
+    games: [],
+  },
+  {
+    title: "Programmer",
+    name: "Max Deurell Kent",
+    linkedin: "linkedin",
+    discord: "discord",
+    website: "website",
+    games: [],
+  },
+  {
+    title: "Programmer",
+    name: "Elin Gammelli",
+    linkedin: "linkedin",
+    discord: "discord",
+    website: "website",
+    games: [],
+  },
+];
+
+const allMembers = [
+  ...ManagementTeamData,
+  ...DesignTeamData,
+  ...ArtTeamData,
+  ...ProgrammersData,
+];
+
+export default {
+  ManagementTeamData,
+  DesignTeamData,
+  ArtTeamData,
+  ProgrammersData,
+  allMembers,
+};
