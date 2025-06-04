a90b70d1ec467c5bfe1004cad153431573385f1e
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-04 17:15:21 +0200

    Small fixes for screen height

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ import TeamColumn from "../../components/TeamColumn";
 
 const AboutPage = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[29px] h-full p-12">
+    <div className="w-full min-h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[29px] h-full p-12 bg-[url(/img/bg-forest.png)] bg-cover bg-center">
       <TeamColumn team="management" />
       <TeamColumn team="design" />
       <TeamColumn team="art" />
