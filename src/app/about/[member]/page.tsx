4781c603447b702f87cfeaa5e799435e856f3a92
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-04 17:57:07 +0200

    Delete [member] folder and add placeholder for T9

--- src/app/about/[member]/page.tsx
+++
@@ -1,6 +0,0 @@
-const MemberPage = () => {
-  return (
-    <div>Member page</div>
-  );
-}
-export default MemberPage;
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,11 +2,14 @@ import TeamColumn from "../../components/TeamColumn";
 
 const AboutPage = () => {
   return (
-    <div className="w-full min-h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[29px] h-full p-12 bg-[url(/img/bg-forest.png)] bg-cover bg-center">
-      <TeamColumn team="management" />
-      <TeamColumn team="design" />
-      <TeamColumn team="art" />
-      <TeamColumn team="programmer" />
+    <div className="w-full min-h-screen">
+      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[29px] h-full p-12 bg-[url(/img/bg-forest.png)] bg-cover bg-center">
+        <TeamColumn team="management" />
+        <TeamColumn team="design" />
+        <TeamColumn team="art" />
+        <TeamColumn team="programmer" />
+      </div>
+     {/* T9 placeholder */}
     </div>
   );
 };
