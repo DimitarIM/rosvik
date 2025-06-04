b98eef760d32f47de217c449d6bc704c40ec86de
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-04 18:00:59 +0200

    Change bg image

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ import TeamColumn from "../../components/TeamColumn";
 
 const AboutPage = () => {
   return (
-    <div className="w-full min-h-screen bg-[url(/img/bg-forest.png)] bg-cover bg-center">
+    <div className="w-full min-h-screen bg-[url(/img/bg_2.png)] bg-cover bg-center">
       <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[29px] h-full p-12 ">
         <TeamColumn team="management" />
         <TeamColumn team="design" />
