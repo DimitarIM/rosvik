0ac43a7bdbb6f296d7fe6e1b2beba7f9d1b0505d
bushra-rauf <bush_rauf@yahoo.com>
2025-06-04 16:50:05 +0200

    responsiveness done

--- src/app/components/MainHeroSection/index.tsx
+++ src/app/components/MainHeroSection/index.tsx
@@ -5,16 +5,16 @@ import { MainHeroSectionProps } from "@/utils/types";
 
 const MainHeroSection = ({image}: MainHeroSectionProps) => { 
     return(
-        <div className={`bg-[url('/img/bg_2.png')] flex flex-col`}>
+        <div className={`bg-[url('/img/bg_2.png')] flex flex-col items-center py-3.5 md:p-20 lg:p-60 `}>
             <Logo />
-            <div className="flex flex-row gap-15 text-center text-[#D7D3CD4D] 
+            <div className="text-2xl flex flex-row gap-5 lg:gap-15 text-center text-[#D7D3CD4D] 
                             lg:text-[46px] sm:text-[40px] md:text-[30px] 
                             font-labrada font-normal lg:mt-[-10px] g:mr-10">
                 <span>Explore</span>
                 <span>Avoid</span>
                 <span>Survive</span>
             </div>
-             <div className="">
+             <div className="w-3xs mt-10">
                 <DownloadButton/>
             </div>   
          </div>   
