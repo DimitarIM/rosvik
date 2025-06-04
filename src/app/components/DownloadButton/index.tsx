0dafc6497c854f09f4109391e05f24b54decf12b
bushra-rauf <bush_rauf@yahoo.com>
2025-06-04 09:15:46 +0200

    fixed the desktop

--- src/app/components/DownloadButton/index.tsx
+++ src/app/components/DownloadButton/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const DownloadButton = () => {
     return (
-        <button className="absolute w-[338px] md:w-[100%] h-[60px] top-[110px] left-[125px] rounded-[30px] flex justify-center hover:bg-[var(--color-sunny)] text-[var(--color-text-neutral-50)] ">
+        <button className="w-[240px] sm:w-[338px] h-[60px] rounded-[30px] flex justify-center  hover:bg-[var(--color-sunny)] text-[var(--color-text-neutral-50)]">
             <Image src="/btn/main_btn_download.png" alt="Download Button" width={369} height={60} />
         </button>
     );
--- src/app/components/Logo/index.tsx
+++ src/app/components/Logo/index.tsx
@@ -4,7 +4,10 @@ import Image from "next/image";
 
 const Logo = ({image}:LogoProps) => {
     return (
-        <div className="absolute w-[1048px] h-[1048px] top-[-101px] right-[1px] opacity-30">
+        <div className="absolute w-[250px] sm:w-[650px] md:w-[1048px] 
+  h-[320px] sm:h-[650px] md:h-[1048px] 
+  mt-[70px] sm:top-[-101px] md:top-[-160px] 
+  left-[210px] sm:right-[1px] md:right-[46px] opacity-30 ">
             <Image src={image}alt="Logo" width={848} height={1048} />
         </div>
     );
