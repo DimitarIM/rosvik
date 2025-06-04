a9ec54486c83bfe00a1809097fd199a1292d2a72
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-04 14:22:42 +0200

    Styling card

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ import TeamColumn from "../../components/TeamColumn";
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[29px] h-full p-12">
       <TeamColumn team="management" />
       <TeamColumn team="design" />
       <TeamColumn team="art" />
--- src/components/TeamColumn/index.tsx
+++ src/components/TeamColumn/index.tsx
@@ -7,16 +7,18 @@ const TeamColumn = ({ team }: TeamColumnProps) => {
   );
 
   return (
-    <div className="bg-foreground font-labrada">
-      <h1 className="text-4xl">
+    <div className="flex flex-col w-full h-full font-labrada space-y-1">
+      <h1 className="text-4xl p-2">
         {team.charAt(0).toUpperCase() + team.slice(1)}
       </h1>
-      {filteredTeam.map((member, index) => (
-        <div key={index}>
-          <p className="text-xl font-bold">{member.title}</p>
-          <p className="text-xl">{member.name}</p>
-        </div>
-      ))}
+      <div className="flex-1 space-y-8 p-7 bg-foreground rounded-[10px]">
+        {filteredTeam.map((member, index) => (
+          <div key={index}>
+            <p className="text-xl font-bold">{member.title}</p>
+            <p className="text-xl underline">{member.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
