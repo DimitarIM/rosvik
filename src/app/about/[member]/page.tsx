4c25264c2781310f7e6c3a17392898537fbaa38d
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-05 16:24:51 +0200

    Make card profile in the middle

--- src/app/about/[member]/page.tsx
+++ src/app/about/[member]/page.tsx
@@ -5,47 +5,60 @@ import { slugify } from "../../../utils/utils";
 
 const MemberPage = ({ params }: MemberParamsProps) => {
   const slugname = params.member;
-  const member = allMembers.find(
-    (person) => slugify(person.name) === slugname
-  );
+  const member = allMembers.find((person) => slugify(person.name) === slugname);
 
   if (!member) {
-    return (
-      <div className="p-10font-labrada">Member not found</div>
-    );
+    return <div className="p-10font-labrada">Member not found</div>;
   }
 
   return (
-    <div className="p-10 space-y-4 font-labrada">
-      <Link href="/about" className="bg-green-800 p-2 rounded-lg">Mock back button</Link>
-      <p className="text-[40px]">{member.name}</p>
-      <p className="text-[40px]">{member.title}</p>
-      <p className="text-sm italic text-muted-foreground">{member.team} Team</p>
-
-      <div className="space-y-2">
-        <a
-          href={member.linkedin}
-          target="_blank"
-          className="text-blue-500 underline"
-        >
-          LinkedIn
-        </a>
-        <br />
-        <a
-          href={member.website}
-          target="_blank"
-          className="text-blue-500 underline"
-        >
-          Website
-        </a>
-        <br />
-        <a
-          href={member.discord}
-          target="_blank"
-          className="text-blue-500 underline"
-        >
-          Discord
-        </a>
+    <div className="font-labrada flex flex-col items-center">
+      <Link href="/about" className="bg-green-800 p-2 rounded-lg">
+        Back (delete later)
+      </Link>
+      <div className="flex w-full lg:w-[60%] justify-between items-center">
+        {/* Left box */}
+        <div className="flex flex-col gap-10">
+          <div>
+            <p className="text-xl lg:text-[40px]">{member.name}</p>
+            <p className="text-xl lg:text-[40px]">{member.title}</p>
+          </div>
+          <div className="w-[90px] md:w-[150px] lg:w-[290px] aspect-square overflow-hidden rounded-full">
+            <img
+              src="/img/profile.png"
+              alt="profile picture"
+              width={100}
+              height={100}
+              className="w-full h-full object-cover object-center"
+            />
+          </div>
+        </div>
+        {/* Right box */}
+        <div>
+          <a
+            href={member.linkedin}
+            target="_blank"
+            className="text-blue-500 underline"
+          >
+            LinkedIn
+          </a>
+          <br />
+          <a
+            href={member.website}
+            target="_blank"
+            className="text-blue-500 underline"
+          >
+            Website
+          </a>
+          <br />
+          <a
+            href={member.discord}
+            target="_blank"
+            className="text-blue-500 underline"
+          >
+            Discord
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ import TeamColumn from "../../components/TeamColumn";
 const AboutPage = () => {
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-10 lg:gap-20 h-full p-12 ">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-10 lg:gap-20 h-full">
         <TeamColumn team="management" />
         <TeamColumn team="design" />
         <TeamColumn team="art" />
