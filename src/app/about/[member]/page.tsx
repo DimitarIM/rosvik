309771768b6458f048f2dec366d706f13b9a5faf
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-06 04:02:24 +0200

    add generateStaticParams

--- src/app/about/[member]/page.tsx
+++ src/app/about/[member]/page.tsx
@@ -1,24 +1,23 @@
 import SingleMemberInfo from "../../../components/SingleMemberInfo";
 import { allMembers } from "../../../data/teamData";
-import { MemberPageProps } from "../../../types/types";
 import { slugify } from "../../../utils/utils";
-import { notFound } from 'next/navigation';
 
-const MemberPage = async ({ params }: MemberPageProps) => {
-  const { slug } = params;
+function generateStaticParams() {
+  return allMembers.map((member) => ({
+    member: slugify(member.name),
+  }));
+}
+
+const MemberPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
+  const {slug} = await params;
+
   const member = allMembers.find((person) => slugify(person.name) === slug);
 
   if (!member) {
-    notFound();
+    return <div className="p-2 font-labrada">Member not found</div>;
   }
 
   return <SingleMemberInfo {...member} />;
 };
 
 export default MemberPage;
-
-export async function generateStaticParams() {
-  return allMembers.map((member) => ({
-    slug: slugify(member.name),
-  }));
-}
