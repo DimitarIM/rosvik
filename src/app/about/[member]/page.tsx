136751430e169f787183412c941a9c81407f5898
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-06 02:30:14 +0200

    Give prop to member

--- src/app/about/[member]/page.tsx
+++ src/app/about/[member]/page.tsx
@@ -1,13 +1,17 @@
 import SingleMemberInfo from "../../../components/SingleMemberInfo";
 import { allMembers } from "../../../data/teamData";
+import { SingleMemberProps } from "../../../types/types";
 import { slugify } from "../../../utils/utils";
 
 const MemberPage = ({ params }: { params: { member: string } }) => {
   const slugname = params.member;
-  const member = allMembers.find((person) => slugify(person.name) === slugname);
+  
+  const member: SingleMemberProps | undefined = allMembers.find(
+    (person) => slugify(person.name) === slugname
+  );
 
   if (!member) {
-    return <div className="p-10font-labrada">Member not found</div>;
+    return <div className="p-2 font-labrada">Member not found</div>;
   }
 
   return (
