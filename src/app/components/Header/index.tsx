286714cfef777d858e0a62ba8dcd8bb1a5560558
bushra-rauf <bush_rauf@yahoo.com>
2025-06-10 01:09:33 +0200

    removed the unnessary link and changed folder to type

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,19 @@
+
+import DesktopNavigation from "../Navigation/DesktopNavigation"
+import MobileNavigation from "../Navigation/MobileNavigation"
+
+const Header = () => {
+    return(
+      <header className="w-full flex top-0 bg-transparent px-6 py-4" >
+        <div className="hidden md:flex items-center space-x-6" >
+        <DesktopNavigation/>
+        </div>
+        {/* mobile navigation */}
+        <div className="md:hidden flex items-center space-x-4">
+            <MobileNavigation/>
+        </div>
+      </header>
+    )
+}
+
+export default Header
--- src/components/MainHeroSection/index.tsx
+++ src/components/MainHeroSection/index.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Logo from "../Logo";
 import DownloadButton from "../DownloadButton";
 
