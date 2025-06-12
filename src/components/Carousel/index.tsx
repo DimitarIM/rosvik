13c65a8a3a94bb61f0d4cc6ce2d2428064002ea5
Dimitar Manastirsky <dmanastirski@gmail.com>
2025-06-13 00:55:26 +0200

    added checkers and dynamic variables

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,23 +1,27 @@
 'use client'
+
 import React, { useState } from 'react'
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 import { SingleMemberProps } from '@/types/types';
-  
-function Carousel({work}:SingleMemberProps) {
-    // if (work === undefined || work.length === 0 ) return null;
-    const images = ["/img/slideshow-img_1.png", "/img/slideshow-img_2.png", "/img/slideshow-img_3.png", "/img/slideshow-img_4.png", "/img/slideshow-img_5.png"];
 
-    const [posIndexes, setPosIndexes] = useState(images.map((_,index) => index));
-    console.log(posIndexes);
+function Carousel({ work }: SingleMemberProps) {
+    // if (work === undefined || work.length === 0) return null;    
+    const images = ["/img/slideshow-img_1.png", "/img/slideshow-img_2.png","/img/slideshow-img_3.png","/img/slideshow-img_4.png"];
+
     const workSize = images.length;
+    let positions: string[] = [];
+    let imageVariants: Record<string, any> = {}
 
+    const [posIndexes, setPosIndexes] = useState(images.map((_, index) => index));
+    const [isLower] = useState<boolean>(workSize < 3)
+
+    //Button logic
 
     const handleNext = () => {
         setPosIndexes((prevIndexes) => {
             const updatedIndexes = prevIndexes.map(
                 (prevIndex) => (prevIndex + 1) % workSize
             );
-            console.log(updatedIndexes)
             return updatedIndexes;
         });
     };
@@ -27,31 +31,70 @@ function Carousel({work}:SingleMemberProps) {
             const updatedIndexes = prevIndexes.map(
                 (prevIndex) => (prevIndex + workSize - 1) % workSize
             );
-            console.log(updatedIndexes)
             return updatedIndexes;
         });
     };
 
-    let positions:string[] = ["center"];
-    let positions_left:string[] = [];
-    let positions_right:string[] = [];
+    if (isLower) {
+        if (workSize !== 1) {
+            positions = ["left0", "right0"];
 
-    for (let i = 0; i < workSize -1; i++) {
-        if (i % 2 === 0) positions_right.push("right" + positions_right.length)
-        else positions_left.push("left" + positions_left.length)
+            imageVariants =
+            {
+                right0: { x: "-50%", scale: 0.8, zIndex: 2 },
+                left0: { x: "50%", scale: 0.8, zIndex: 2 },
+            }
+        }
+        else {
+            positions = ["center"];
+
+            imageVariants =
+            {
+                center: { x: "0", scale: 1, zIndex: 2 }
+            }
+        }
     }
+    else {
+        //SetPositions    
+        positions = ["center"];
+        let positions_left: string[] = [];
+        let positions_right: string[] = [];
 
-    positions = positions.concat(positions_left.concat(positions_right));
+        for (let i = 0; i < workSize - 1; i++) {
+            if (i % 2 === 0) positions_right.push("right" + positions_right.length)
+            else positions_left.push("left" + positions_left.length)
+        }
+        positions = positions.concat(positions_left.concat(positions_right));
+
+        //SetVariants
+        imageVariants =
+        {
+            center: { x: "0%", scale: 1, zIndex: 5 },
+            //  right1: { x: "100%", scale: 0.8, zIndex: 3 },
+            //  right0: { x: "200%", scale: 0.8, zIndex: 2 },
+            // left0: { x: "-100%", scale: 0.8, zIndex: 2 },
+            //  left1: { x: "-200%", scale: 0.8, zIndex: 3 },
+        }
+
+        for (let i = 1; i <= positions_left.length; i++) {
+            imageVariants[positions_left[i - 1]] = {
+                x: `-${i * 100}%`, scale: 0.8, zIndex: 1 + i
+            }
+        }
+
+        for (let i = 0; i < positions_right.length; i++) {
+            imageVariants[positions_right[i]] = {
+                x: `${(positions_right.length - i) * 100}%`, scale: 0.8, zIndex: positions_right.length - i
+            }
+        }
+    }
+    console.log(imageVariants);
     console.log(positions);
-    const imageVariants = {
-        center: { x: "0%", scale: 1, zIndex: 5 },
-        right0: { x: "100%", scale: 0.8, zIndex: 2 },
-        right1: { x: "200%", scale: 0.8, zIndex: 3 },
-        left0: { x: "-100%", scale: 0.8, zIndex: 2 },
-        left1: { x: "-200%", scale: 0.8, zIndex: 3 },
-    };
+
     return (
+        
         <div className="relative overflow-hidden flex items-center flex-col justify-center w-full h-full">
+            <h3 className='text-5xl'>Portfolio</h3>
             {images.map((image, index) => (
                 <motion.img
                     key={index}
@@ -66,16 +109,17 @@ function Carousel({work}:SingleMemberProps) {
                 />
             ))}
             <div className="mt-[300px] sm:mt-[400px] md:mt-[400px] lg:mt-[600px] xl:mt-[1000px] flex flex-row gap-3 ">
-                <button
-                    onClick={handleBack}
-                >
-                    <img className='w-full h-full' src="/btn/arrow-btn_left.png" alt="" />
-                </button>
-                <button
-                    onClick={handleNext}
-                >
-                    <img src="/btn/arrow-btn_right.png" alt="" />
-                </button>
+                {
+                    isLower ? null :
+                        <>
+                            <button onClick={handleBack}>
+                                <img className='w-full h-full' src="/btn/arrow-btn_left.png" alt="" />
+                            </button>
+                            <button onClick={handleNext}>
+                                <img src="/btn/arrow-btn_right.png" alt="" />
+                            </button>
+                        </>
+                }
             </div>
         </div>
     );
