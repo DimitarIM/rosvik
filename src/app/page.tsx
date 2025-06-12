d89f5bacd1f3d91d2f837f0679176a4cfd39f2ec
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-12 20:01:17 +0200

    Fix home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import KeyFeatures from "@/components/KeyFeatures"
-import { keyfeaturesData } from "@/data/keyfeaturesdata"
+import KeyFeatures from "@/components/KeyFeatures";
+import { keyfeaturesData } from "@/data/keyfeaturesdata";
 import MainHeroSection from "../components/MainHeroSection";
 
 export default function Home() {
   return (
     <>
-      <MainHeroSection firstText="Explore" secondText="Avoid" thirdText="Survive"/>
+      <MainHeroSection
+        firstText="Explore"
+        secondText="Avoid"
+        thirdText="Survive"
+      />
       <KeyFeatures keyfeatures={keyfeaturesData} />
     </>
   );
