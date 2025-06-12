e3a2ae9c2d19b5a7b60f240dd6a84572a5b74bd4
bushra-rauf <bush_rauf@yahoo.com>
2025-06-12 02:46:53 +0200

    added mt

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -3,7 +3,7 @@ import { LogoProps } from "@/types/types";
 
 const Logo = ({image,alt}: LogoProps) => {
     return (
-        <div className="opacity-30 ">
+        <div className="opacity-30 mt-15">
           <Image src={image} alt={alt} width={704} height={275.12} />
         </div>
     );
