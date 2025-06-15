41ae6a63e8e27710abd0a705a5bf22d7114e8f04
Srilatha <119068383+potnurusrilatha@users.noreply.github.com>
2025-06-15 23:18:40 +0200

    fixed mbl

--- src/components/KeyFeatures/index.tsx
+++ src/components/KeyFeatures/index.tsx
@@ -3,21 +3,21 @@ import { keyfeaturesProps } from "@/types/types";
 
 const KeyFeatures = ({ keyfeatures }: keyfeaturesProps) => {
   return (
-    <section className="flex flex-col items-center justify-center py-12 px-4 font-labrada overflow-hidden">
-      <h2 className="text-[32px] md:text-[40px] text-feature font-semibold mb-8 text-center mt-60">
+    <section className="flex flex-col items-center justify-center py-12 px-16 gap-2 font-labrada overflow-hidden">
+      <h2 className="text-[20px] md:text-[40px] text-feature font-semibold md:mb-2 sm:mb-2 p-2 text-center mt-30">
         Key Features
       </h2>
-      <div className="bg-foreground text-feature backdrop-blur-sm rounded-xl md:px-6 md:py-8 w-full max-w-[800px]">
-        <div className="flex flex-col md:flex-row md:flex-wrap md:justify-center gap-6 text-lg md:text-xl text-center md:text-left">
+      <div className="bg-foreground text-feature backdrop-blur-sm rounded-xl gap:2 md:px-6 md:py-8 max-w-[90%]w-full sm:max-w-[600px] md:max-w-[800px]">
+        <div className="flex flex-col md:flex-row md:flex-wrap md:justify-center text-lg md:text-xl text-center p-4">
           {keyfeatures.map((feature, index) => (
             <div
               key={index}
-              className="flex items-center justify-start sm:gap-4 text-[24px] md:text-[24px]"
+              className="flex items-center justify-start md:gap-2 text-[12px] md:text-[26px]"
             >
-              <span className="font-labrada text-xs block md:hidden">◆</span>
+              <span className="font-labrada text-[12px] block md:hidden pr-2">◆</span>
               <span>{feature}</span>
               {index < keyfeatures.length - 1 && (
-                <span className="text-xs hidden md:inline font-labrada">◆</span>
+                <span className="text-[12px] hidden md:inline font-labrada pl-2">◆</span>
               )}
             </div>
           ))}
