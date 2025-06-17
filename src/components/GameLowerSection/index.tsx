1ecb3416383c7e2a332231a1982997b3942f9334
bushra-rauf <bush_rauf@yahoo.com>
2025-06-17 18:59:54 +0200

    add background and styling done on mobile

--- src/components/GameLowerSection/index.tsx
+++ src/components/GameLowerSection/index.tsx
@@ -3,9 +3,10 @@ import bgImage from '../../../public/img/about_sticky.png'
 import { GameLowerSectionProps } from "@/types/types";
 const GameLowerSection = ({title, description}:GameLowerSectionProps) => {
     return (
-       <div className="relative "> 
+       <div className="relative pb-0.5"> 
         <Image src={bgImage} alt='background image' className="w-full"/>
-        <div className="w-[70%] py-12 my-8 mx-auto bg-foreground text-color-primary rounded-[10px] shadow-lg opacity-0.3 md:absolute top-12 left-25 lg:left-43 md:w-[35%] md:h-[45%]">
+        <div className=" bg-[url('/img/about_game_bg-mobile.png')] bg-no-repeat bg-cover 
+        md:bg-none w-[70%] px-2 py-12 my-8 mx-auto bg-foreground text-color-primary rounded-[10px] shadow-lg opacity-100 md:absolute top-12 left-25 lg:left-43 md:w-[35%] md:h-[45%]">
             <div className='pl-7 '>
                 <p>{title}</p>
                 <p>{description}</p>
