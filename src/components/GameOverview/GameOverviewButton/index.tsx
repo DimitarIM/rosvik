4691e6baed17ca3d5abeefd1a930c433ffa5e668
Dimitar Manastirsky <dmanastirski@gmail.com>
2025-06-19 00:10:57 +0200

    changed gameOverview link to _blank

--- src/components/GameOverview/GameOverviewButton/index.tsx
+++ src/components/GameOverview/GameOverviewButton/index.tsx
@@ -5,7 +5,7 @@ import { DownLoadButtonProps } from "@/types/types"
 const GameOverviewButton = ({downloadButtonLink, downloadButtonText}: DownLoadButtonProps) => {
     return (
         <div className="sm:grid  sm:text-center sm:rounded-full sm:bg-button-color sm:h-11 sm:min-w-[240px] lg:h-[60px] lg:w-[350px] xl:w-[369px]">
-            <button className=" sm:text-[18px] lg:text-[22px] xl:text-[26px]" onClick={() => window.open(downloadButtonLink, "_self")}>
+            <button className=" sm:text-[18px] lg:text-[22px] xl:text-[26px]" onClick={() => window.open(downloadButtonLink, "_blank")}>
                 {downloadButtonText}
             </button>
         </div>
