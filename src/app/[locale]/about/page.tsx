39c04604867d4cb2547cef82e749280f3aedc5c1
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-20 20:09:18 +0700

    fix translations in sv

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,7 +1,9 @@
 import TeamColumn from "../../../components/TeamColumn";
 import FooterLogoSection from "@/components/FooterLogoSection";
+import { useTranslations } from "next-intl";
 
 const AboutPage = () => {
+  const t = useTranslations("about");
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-10 h-full p-10">
@@ -16,12 +18,7 @@ const AboutPage = () => {
           logo="/img/team_fg_img.png"
           description={
             <div className="text-left p-7">
-              <p className="flex flex-col">
-                A team of ambitious students dedicated to creating challenging,
-                complex, or technically ambitious projects. Our work is often
-                marked by creativity, resilience, and a passion for pushing the
-                limits of interactive entertainment.
-              </p>
+              <p className="flex flex-col">{t("lowerSection")}</p>
             </div>
           }
         />
