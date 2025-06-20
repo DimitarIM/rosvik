0247ff76fb4a23219451b573cfa6d64287cc1033
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-21 01:26:42 +0700

    take optional out from GameOverViewProps

--- src/types/types.ts
+++ src/types/types.ts
@@ -7,15 +7,15 @@ export type keyfeaturesProps = {
 
 export type GameOverViewProps = {
     bgImageAlt: string;
-    pageTitle?: string;
+    pageTitle: string;
     aboutText: string[];
     imageOneSrc: string;
     imageOneAlt: string;
     imageTwoSrc: string;
     imageTwoAlt: string;
-    gameLoopTitle?: string;
-    gameLoopText?: string[];
-    managementBoxText?:string[];
+    gameLoopTitle: string;
+    gameLoopText: string[];
+    managementBoxText:string[];
 }
 
 export type DownLoadButtonProps = {
