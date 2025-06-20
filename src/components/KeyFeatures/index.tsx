f2e5eb5475e0f8231ac2124b405f4e65d6e4d6a5
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-21 02:24:04 +0700

    adjust key features font siz

--- src/components/KeyFeatures/index.tsx
+++ src/components/KeyFeatures/index.tsx
@@ -6,14 +6,14 @@ const KeyFeatures = ({ keyfeatures }: keyfeaturesProps) => {
   const t = useTranslations("home");
   return (
     <section className="relative flex flex-col items-center justify-center py-12 px-4 font-labrada overflow-hidden z-20">
-      <h2 className="text-[40px] font-labrada text-feature md:text-[40px] mb-8 text-center">
+      <h2 className="text-3xl font-labrada text-feature md:text-[40px] mb-8 text-center">
         {t("keyfeatures.header")}
       </h2>
       <div className="bg-foreground text-feature rounded-xl py-3 md:py-9 md:px-20 w-full max-w-[320px] md:w-full md:max-w-6xl">
         <div className="flex flex-col items-center md:flex-row text-nowrap justify-center gap-3 md:gap-[2.5vw] lg:gap-15 text-lg md:text-xl text-center md:text-left">
           {keyfeatures.map((feature, index) => (
             <Fragment key={index}>
-              <div className=" text-[20px] md:text-[24px] w-[266px] text-start md:text-center">
+              <div className=" text-[20px] w-[266px] text-start md:text-center">
                 <span className="font-labrada text-xs inline md:hidden md:pr-0 ">
                   ◆
                 </span>
