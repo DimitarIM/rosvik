c4d228ff1859840f3f4c65aa6904a72cc9061285
Dimitar Manastirsky <dmanastirski@gmail.com>
2025-06-23 11:27:51 +0200

    changed text size and padding to fit better on mobile

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -7,7 +7,7 @@ export default function AboutPageLayout({
 }) {
   return (
     <Inner>
-      <div className="relative w-full min-h-screen bg-[url(/img/bg_2.png)] bg-cover bg-center pt-[130px] md:pt-[170px]">
+      <div className="relative w-full min-h-screen bg-[url(/img/bg_2.png)] bg-cover bg-center pt-[100px] md:pt-[170px]">
         <div className="absolute inset-0 bg-black/20 z-0"></div>
         {children}
       </div>
