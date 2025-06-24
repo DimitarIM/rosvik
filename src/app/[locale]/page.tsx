a83d1cac82a171270fc2cc127ba105d7eb1c0592
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-24 22:35:20 +0700

    delete use client

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import Inner from "@/components/Layout/Inner";
 import MainHeroSection from "../../components/MainHeroSection";
 import MediaTrailer from "@/components/MediaTrailer";
