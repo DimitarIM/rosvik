6045512807cab4c864785b5635931d015082e587
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-25 16:09:40 +0700

    localePrefix: 'always',

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,6 +3,5 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["en", "sv"],
   defaultLocale: "en",
-  localeCookie: false,
-  localePrefix: 'never',
+  localePrefix: 'always',
 });
