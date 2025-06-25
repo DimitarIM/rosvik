0e4c55c847c738e4fb0508365a38756ccb28c5e7
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-25 12:14:26 +0700

    Change back to old middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,8 @@
 import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
 
-
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: [
-    '/',
-    `/${routing.locales[0]}/:path*`,
-    `/${routing.locales[1]}/:path*`,
-    '/((?!api|_next|favicon.ico|.*\\..*).*)',
-  ],
+  matcher: ['/((?!api|_next|.*\\..*).*)']
 };
