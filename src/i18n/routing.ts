027b20e308e387b9d8b3788cb0fcec26c6c39299
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-25 12:09:13 +0700

    Change default lang to sv

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,6 +2,6 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   locales: ["en", "sv"],
-  defaultLocale: "en",
+  defaultLocale: "sv",
   localePrefix: 'always'
 });
