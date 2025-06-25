9fa0729cb9bc92413493e1cd79a813b3616a3bcb
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-25 16:15:57 +0700

    never

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,7 +3,7 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["en", "sv"],
   defaultLocale: "en",
-  localePrefix: 'as-needed',
+  localePrefix: 'never',
   localeCookie: true,
   localeDetection: true
 });
