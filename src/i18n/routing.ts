e62fd3601f82c2b7058d8d554c0871306b64a9bb
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-25 15:50:46 +0700

    take localeDetection to true

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,6 +3,6 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["en", "sv"],
   defaultLocale: "en",
-  localeDetection: false,
+  localeDetection: true,
   localePrefix: 'never'
 });
