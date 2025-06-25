d101b5a4d6f1c5b70e43273a360442b792ad9f92
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-25 12:34:17 +0700

    old middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,8 @@
 import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
 
-const nextIntlMiddleware = createMiddleware(routing);
-
-export default async function middleware(req: any) {
-
-  const response = nextIntlMiddleware(req);
-
-  return response;
-}
+export default createMiddleware(routing);
 
 export const config = {
-
-  matcher: [
-    '/',
-    '/en/:path*',
-    '/sv/:path*',
-    '/((?!api|_next|favicon.ico|.*\\..*).*)',
-  ],
+  matcher: ['/((?!api|_next|.*\\..*).*)']
 };
