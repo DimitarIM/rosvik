7de5c1f057eeaba50f4dc008c26e46daf6b13e66
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-25 12:19:43 +0700

    change default lagn to en

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,7 +2,7 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   locales: ["en", "sv"],
-  defaultLocale: "sv",
+  defaultLocale: "en",
   localeDetection: false,
   localePrefix: 'always'
 });
