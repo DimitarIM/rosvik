b3178643cc1261c873eb2cfb26abf6165c3c5e8b
May Sunktong <pattaranan.sunktong@gmail.com>
2025-06-25 12:13:20 +0700

    Not working on local, try new middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,14 @@
 import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
 
+
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: ['/',
-  '/en/:path*',
-  '/sv/:path*',
-  '/((?!api|_next|favicon.ico|.*\\..*).*)',]
+  matcher: [
+    '/',
+    `/${routing.locales[0]}/:path*`,
+    `/${routing.locales[1]}/:path*`,
+    '/((?!api|_next|favicon.ico|.*\\..*).*)',
+  ],
 };
